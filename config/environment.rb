1235f2664f6f80bdaa1ecb78c52441a3ab9ef595
Navid Matin <seyedmatin.1@osu.edu>
2013-11-14 14:01:31 -0500

    Initial Commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Cse3901Project6::Application.initialize!
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cse3901Project6::Application.config.session_store :cookie_store, key: '_cse3901_Project6_session'
